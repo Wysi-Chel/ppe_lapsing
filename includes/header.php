2c59906d0a05005ef626dfb819b4dc65fd304dc4
Chel <iam.chel1021@gmail.com>
2026-05-28 16:11:25 +0800

    remove header

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 $pageTitle = $pageTitle ?? APP_NAME;
 $pageHeading = $pageHeading ?? $pageTitle;
 $pageDescription = $pageDescription ?? 'Keep your PPE schedules, assets, and analysis in one place.';
+$documentTitle = $pageTitle === APP_NAME ? APP_NAME : $pageTitle . '  ' . APP_NAME;
 $loggedInUser = current_user();
 $showShell = $loggedInUser !== null;
 ?>
@@ -12,7 +13,7 @@ $showShell = $loggedInUser !== null;
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= e($pageTitle) ?> | <?= e(APP_NAME) ?></title>
+    <title><?= e($documentTitle) ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet">
